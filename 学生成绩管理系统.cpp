19464b00374844181bc1e8aa852189b522d52a27
lai-yuhang <121168171+lai-yuhang@users.noreply.github.com>
2023-01-02 15:12:50 +0800

    Update 学生成绩管理系统.cpp

--- 学生成绩管理系统.cpp
+++ 学生成绩管理系统.cpp
@@ -500,3 +500,301 @@ void save_score()
 
 
 }
+//读取文件信息
+void readList()
+{
+	FILE* fp;
+	fp = fopen("student.txt ", "r");
+	if (fp == NULL)
+	{
+		printf("打开文件失败！\n");
+	}
+
+	while (!feof(fp))
+	{
+		fscanf(fp, "%s %s %s %s\n",
+			s[temp_number].id,
+			s[temp_number].name,
+			s[temp_number].sex,
+			s[temp_number].pwd);
+		temp_number++;
+	}
+	fclose(fp);
+	printf("读取文件信息成功！\n");
+}
+//读取成绩信息
+void read_score()
+{
+	FILE* fp;
+	fp = fopen("score.txt ", "r");
+	if (fp == NULL)
+	{
+		printf("打开文件失败！\n");
+	}
+
+	while (!feof(fp))
+	{
+		fscanf(fp, "%s %f %f\n",
+			score[number].id,
+			&score[number].chinese,
+			&score[number].math);
+		number++;
+	}
+	fclose(fp);
+	printf("读取文件信息成功！\n");
+}
+//按成绩排序
+void sort()
+{
+	for (int i = 0; i < number; i++)
+	{
+		for (int j = 0; j < number - 1 - i; j++)
+		{
+			if (score[j].chinese + score[j].math > score[j + 1].chinese + score[j + 1].math)
+			{
+				Student p;
+				p = s[j];
+				s[j] = s[j + 1];
+				s[j + 1] = p;
+			}
+		}
+	}
+	printf("排序成功\n");
+	for (int i = 0; i < number; i++)
+	{
+		printf("学号：%s\t", score[i].id);
+		printf("语文成绩：%0.2f\t", score[i].chinese);
+		printf("数学成绩：%0.2f\n", score[i].math);
+	}
+}
+
+
+
+//查询自身信息
+void show_self(int index)
+{
+
+	printf("学号：%s\t", s[index].id);
+	printf("姓名：%s\t", s[index].name);
+	printf("性别：%s\t", s[index].sex);
+	printf("密码：%s\n", s[index].pwd);
+
+}
+//查询自身成绩
+void show_self_score(int index)
+{
+
+	for (int i = 0; i < number; i++)
+	{
+		if (!strcmp(s[index].id, score[i].id))
+		{
+			printf("学号：%s\t", score[i].id);
+			printf("语文成绩：%0.2f\t", score[i].chinese);
+			printf("数学成绩：%0.2f\n", score[i].math);
+		}
+	}
+
+
+}
+//修改密码
+void update_self(int index)
+{
+	printf("请输入新的密码：\n");
+	scanf("%s", s[index].pwd);
+	printf("修改成功！\n");
+
+
+}
+
+//主函数
+int main()
+{
+
+	int chioce;
+
+	while (1)
+	{
+		one:
+		main_menu();
+		int ch;
+		printf("请输入你的选择：\n");
+		scanf("%d", &ch);
+		if (ch == 1)
+		{
+			teacher_do_menu();
+			printf("请输入你的选择：\n");
+			scanf("%d", &ch);
+			if (ch == 1)
+			{
+				int flag = teacher_login();
+				if (flag == 1)
+				{
+					while (1)
+					{
+						teacher_menu();
+						printf("请输入你的选择（0-9）:\n");
+						scanf("%d", &chioce);
+						if (chioce < 0 || chioce>9)
+						{
+							printf("输入有误，请重新输入！\n");
+						}
+						else
+						{
+							switch (chioce)
+							{
+							case 1:
+								add();
+								break;
+							case 2:
+								del();
+								break;
+							case 3:
+								modify();
+								break;
+							case 4:
+								showAll();
+								break;
+							case 5:
+								countList();
+								break;
+							case 6:
+								readList();
+								break;
+							case 7:
+								saveList();
+								break;
+							case 8:
+								search();
+								break;
+							case 9:
+								sort();
+								break;
+							case 0:
+								goto one;
+								break;
+							}
+						}
+
+					}
+				}
+			}
+			else if(ch==2)
+			{
+				int flag = teacher_login();
+				if (flag == 1)
+				{
+					while (1)
+					{
+						score_menu();
+						printf("请输入你的选择（0-9）:\n");
+						scanf("%d", &chioce);
+						if (chioce < 0 || chioce>9)
+						{
+							printf("输入有误，请重新输入！\n");
+						}
+						else
+						{
+							switch (chioce)
+							{
+							case 1:
+								add_score();
+								break;
+							case 2:
+								del_score();
+								break;
+							case 3:
+								modify_score();
+								break;
+							case 4:
+								show_score();
+								break;
+							case 5:
+								countList();
+								break;
+							case 6:
+								read_score();
+								break;
+							case 7:
+								save_score();
+								break;
+							case 8:
+								search_score();
+								break;
+							case 9:
+								sort();
+								break;
+							case 0:
+								goto one;
+								break;
+							}
+						}
+
+					}
+				}
+			}
+			else if (ch == 0)
+			{
+				goto one;
+			}
+			else
+			{
+				printf("输入有误！\n");
+			}
+
+
+
+		}
+		else if (ch == 2)
+		{
+			int index = stu_login();
+			if (index != -1)
+			{
+				while (1)
+				{
+					student_menu();
+					printf("请输入你的选择（0-4）:\n");
+					scanf("%d", &chioce);
+					if (chioce < 0 || chioce>4)
+					{
+						printf("输入有误，请重新输入！\n");
+					}
+					else
+					{
+						switch (chioce)
+						{
+						case 1:
+							show_self(index);
+							break;
+						case 2:
+							show_self_score(index);
+							break;
+						case 3:
+							update_self(index);
+							break;
+						case 0:
+							printf("欢迎下次使用！\n");
+							goto one;
+							break;
+						}
+					}
+
+				}
+			}
+
+
+		}
+		else if (ch == 0)
+		{
+			printf("欢迎下次使用！\n");
+			exit(0);
+		}
+		else
+		{
+			printf("输入有误！\n");
+		}
+	}
+
+
+	return 0;
+}
+
